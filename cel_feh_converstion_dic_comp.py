e06bef2c400b3ab578fb2056a393acae16afdcc9
savi3karanth <Karanth.savi3@gmail.com>
2025-02-28 14:19:52 -0500

    using the dictionary comp converted the c to f from dictionary and created the new dict for f

---
+++ cel_feh_converstion_dic_comp.py
@@ -0,0 +1,17 @@
+# You are going to use Dict Comprehension to create dictionary called weather_f
+# that takes each temperature in degrees Celsius and converts it into degrees Fahrenheit.
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+
+weather_f = {day: (temp_c * 9/5) + 32 for (day, temp_c) in weather_c.items()}
+print(weather_f)
+
+
