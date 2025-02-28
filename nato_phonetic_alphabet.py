4e92559d2dbc476b141cbc8d5565f17c2e9606fc
savi3karanth <Karanth.savi3@gmail.com>
2025-02-28 14:49:02 -0500

    from CSV file created phonetic alphabet dictionary when the user type the word it gives the list with phonetic alphabet info

---
+++ nato_phonetic_alphabet.py
@@ -0,0 +1,19 @@
+import pandas
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+print(data.to_dict())
+print(data)
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+{"A": "Alfa", "B": "Bravo"}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter the word: ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
+
